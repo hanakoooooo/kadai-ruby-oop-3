47daa72d867ee4029ff4f6a204395f0a1882c276
hanako.yamada <h.yamada20210929@gmail.com>
2021-10-08 01:20:43 +0000

    add kadai

---
+++ human.rb
@@ -0,0 +1,15 @@
+require './thinkable'
+require './animal'
+
+class Human < Animal
+ attr_accessor :hobby
+
+ include Thinkable
+
+ def initialize(name,age,hobby)
+  self.name = name
+  self.age = age
+  self.hobby = hobby
+ end
+ 
+end
